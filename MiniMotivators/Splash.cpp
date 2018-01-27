272a6274acf3026a4d24ab11c07efae62660f449
Greg Cahill <gamejam@L217C102>
2018-01-27 15:14:33 +0000

    mmm

--- MiniMotivators/Splash.cpp
+++ MiniMotivators/Splash.cpp
@@ -1,7 +1,5 @@
 #include"Splash.h"
-#include "Game.h"
-#include "GameStates.h"
-
+#include"game.h"
 Splash::Splash()
 {
 }
@@ -34,13 +32,14 @@ void Splash::setup(sf::Font & t_font)
 	m_message.setFillColor(sf::Color::Green);
 	m_message.setPosition(sf::Vector2f{ 50.0f,50.0f });
 	m_message.setCharacterSize(24u);
-	m_message.setString("'Looking'~Corita Kent/ press any key to go to menu");
+	m_message.setString("'Fuck the mainstream!'\n press any key to go to menu");//yurt
 	m_message.setFont(m_font);
 
-	if (!m_texture.loadFromFile("ASSETS//CAPTURE.png"))
+	if (!m_texture.loadFromFile("ASSETS\\Capture.PNG"))
 	{
 		// simple error message if previous call fails
 		std::cout << "problem loading sprite" << std::endl;
 	}
 	m_sprite.setTexture(m_texture);
+	m_sprite.setPosition(175, 100);
 }

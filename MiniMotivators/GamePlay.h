3c7135689c297e1aee7acb5196ddc4eb8a2d5c6b
(Student) - Martin Farrell <gamejam@L217C103>
2018-01-27 16:36:16 +0000

    GamePlay class

--- MiniMotivators/GamePlay.h
+++ MiniMotivators/GamePlay.h
@@ -1,1 +1,17 @@
 #pragma once
+#include"SFML\Graphics.hpp"
+class GamePlay
+{
+public:
+	GamePlay();
+	~GamePlay();
+	void update(sf::Time t_deltaTime);
+	void render(sf::RenderWindow &t_window);
+	void setup(sf::Font &t_font);
+private:
+	sf::Font m_font;
+	sf::Text m_message;
+	int m_timer = 0;
+	sf::Texture m_texture;
+	sf::Sprite m_sprite;
+};

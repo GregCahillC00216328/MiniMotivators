67cf81d648c3c55f3791d9471445725f644dbc06
Greg Cahill <gamejam@L217C102>
2018-01-28 11:01:53 +0000

    Added mouse detection for players

--- MiniMotivators/Level.h
+++ MiniMotivators/Level.h
@@ -15,6 +15,8 @@ private:
 	sf::Text m_message;
 	sf::Sprite m_kidSprite;
 	sf::Texture m_kidTexture;
+	sf::Texture m_mamTexture;
+	sf::Texture m_dadTexture;
 	sf::Texture m_shirtTexture;
 
 	sf::RectangleShape m_mam[4];
@@ -24,6 +26,6 @@ private:
 	sf::RectangleShape m_sequenceSquares[5]; //squares used to display sequence/missing kids
 	sf::RectangleShape m_fatherSquares[5];
 	sf::RectangleShape m_motherSquares[5];
-	sf::RectangleShape m_childSquares[5];
+	sf::RectangleShape m_kidSquares[5];
 	sf::RectangleShape m_shirtSquare[5];
 };
